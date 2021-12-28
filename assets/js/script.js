1d6b092cf3bcdd7721c96c7475f91f2e77525d53
Tgarrey37 <tpopp0303@gmail.com>
2021-12-27 18:44:01 -0700

    Added five card forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,27 +13,32 @@ function currentWeatherAPI(city) {
       var imageSource = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
       var img = document.createElement("img");
       img.setAttribute("src", imageSource);
-      var currentWeatherDIV = document.querySelector("#currentWeather");
-      currentWeatherDIV.append(img);
+      // var currentWeatherDIV = document.querySelector("#currentWeather");
+      // currentWeatherDIV.append(img);
 
       // searchButton.addEventListener("click", function () {
       var card = document.getElementById("weatherCard");
       card.innerHTML = "";
-      var forecastDate = document.createElement("h1");
-      forecastDate.textContent = 
-      var cityName = document.createElement("h2");
+      var forecastDate = document.createElement("h3");
+      forecastDate.textContent = moment().format("L"); 
+      var cityName = document.createElement("h1");
       cityName.textContent = data.name;
       var tempEl = document.createElement("p");
-       tempEl.textContent = data.main.temp;
+       tempEl.textContent = "Temp: " + data.main.temp + " °F";
       var humidityEl = document.createElement("p");
-      humidityEl.textContent = data.main.humidity;
+      humidityEl.textContent = "Humidity: " + data.main.humidity + " %";
       var windEl = document.createElement("p");
-      windEl.textContent = data.wind.speed;
+      windEl.textContent = "Wind Speed: " + data.wind.speed + " MPH";
       
-      card.append(cityName, humidityEl, tempEl, windEl);
-      // humidityEl.append("#weatherCard");
-      console.log(humidityEl);
-      // });
+      var saveSearch = function(){
+        localStorage.setItem("cityName", JSON.stringify(cityName));
+    };
+      // var uvIndex = document.createElement("p");
+      // uvIndex.textContent = "UV Index:" + data.current.uvi;
+      
+      card.append(cityName, forecastDate,img,tempEl, windEl, humidityEl);
+     
+      
       //   card current weather
     });
 }
@@ -48,15 +53,41 @@ function oneCallAPI(latitude, longitude) {
     })
     .then(function (data) {
       console.log(data);
-    });
+    
+      
+
+    var fiveDayForecast = document.getElementById("fiveDayForecast");
+    fiveDayForecast.innerHTML="";
+    for (var i=0; i < 5; i++){
+      var card = document.createElement("div");
+      var column = document.createElement("div");
+      var cardBody = document.createElement("div");
+      card.setAttribute("class", "card");
+      column.setAttribute("class", "col");
+      cardBody.setAttribute("class", "card-body");
+      var tempEl = document.createElement("p");
+      tempEl.textContent= "Temp: " + data.daily[i].temp.max + " °F";
+      var humidityEl = document.createElement("p");
+      humidityEl.textContent = "Humidity: " + data.daily[i].humidity + " %";
+      var windEl = document.createElement("p");
+      windEl.textContent = "Wind Speed: " + data.daily[i].wind_speed + " MPH";
+      cardBody.append(tempEl,windEl,humidityEl,);
+      card.append(cardBody);
+      column.append(card);
+      fiveDayForecast.append(column);
+    }
+
+    // fiveDayForecast.textContent = data.daily.temp;
+    // card.append()
+
   // uvi, color and 5 day forecast
+});
 }
 
 searchButton.addEventListener("click", function () {
   var cityInput = document.getElementById("cityInput").value;
+  var weatherCard = document.querySelector("#card");
+  weatherCard.classList.remove("hidden");
   currentWeatherAPI(cityInput);
 });
-//   var humidityEl = data.main.humidity;
-//   humidityEl.append("weatherCard");
-//   console.log(humidityEl);
-// });
+
